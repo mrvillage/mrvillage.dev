0a644b920abf9892bbc15291f0122063fe320daf
mrvillage <mr.village05@gmail.com>
2023-03-03 09:33:48 -0500

    Add some more config options

--- app/routes/latex.tsx
+++ app/routes/latex.tsx
@@ -1,8 +1,13 @@
 import katex from "katex";
 import type { ActionFunction } from "@remix-run/cloudflare";
 
-export const action: ActionFunction = async ({ request }) => {
+export const action: ActionFunction = async ({
+  request,
+  params: { display },
+}) => {
   return katex.renderToString(await request.text(), {
     throwOnError: false,
+    displayMode: display === "display",
+    output: "html",
   });
 };
