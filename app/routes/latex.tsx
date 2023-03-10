95bab6b40a38d80ff17a3ebaf88a25db5115ef13
mrvillage <mr.village05@gmail.com>
2023-03-10 09:44:56 -0500

    Add GET mode for latex

--- app/routes/latex.tsx
+++ app/routes/latex.tsx
@@ -1,13 +1,10 @@
 import katex from "katex";
 import { json } from "@remix-run/cloudflare";
-import type { ActionFunction } from "@remix-run/cloudflare";
+import type { ActionFunction, LoaderFunction } from "@remix-run/cloudflare";
 
-export const action: ActionFunction = async ({
-  request,
-  params: { display },
-}) => {
+function createRes(text: string, display: string | undefined): Response {
   const res = json({
-    html: katex.renderToString(await request.text(), {
+    html: katex.renderToString(text, {
       throwOnError: false,
       displayMode: display === "display",
       output: "html",
@@ -17,4 +14,17 @@ export const action: ActionFunction = async ({
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type");
   return res;
+}
+
+export const action: ActionFunction = async ({
+  request,
+  params: { display },
+}) => {
+  return createRes(await request.text(), display);
+};
+
+export const loader: LoaderFunction = async ({
+  params: { display, latex },
+}) => {
+  return createRes(latex || "", display);
 };
