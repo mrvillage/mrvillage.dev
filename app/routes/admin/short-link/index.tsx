7f193fe57d281c50d96b13f5d20124d52f782219
mrvillage <josef@mrvillage.dev>
2023-12-16 22:35:34 -0500

    Delete old site

--- app/routes/admin/short-link/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { LoaderFunction } from "@remix-run/cloudflare";
-import { adminsOnly } from "~/utils/routes";
-
-export const loader: LoaderFunction = adminsOnly(() => ({}));
-
-export default function Index() {
-  return <></>;
-}
