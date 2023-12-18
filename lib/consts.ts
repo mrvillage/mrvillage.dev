72caf7ea49574f2931a7f18d430abdad3eb52056
mrvillage <josef@mrvillage.dev>
2023-12-18 09:45:51 -0500

    Redo link constants to use the actual links, will save a roundtrip Worker request

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,4 +1,4 @@
-export const GITHUB_LINK = "https://mrvillage.dev/s/github";
-export const LINKEDIN_LINK = "https://mrvillage.dev/s/linkedin";
+export const GITHUB_LINK = "https://github.com/mrvillage";
+export const LINKEDIN_LINK = "https://linkedin.com/in/josef-graf";
 export const EMAIL_LINK = "mailto:josef@mrvillage.dev";
-export const V19_LINK = "https://mrvillage.dev/s/v19";
+export const V19_LINK = "https://v19.io";
