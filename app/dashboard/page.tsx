9422d0c8c379108a95d0515df32ed6ad3ecc860a
mrvillage <josef@mrvillage.dev>
2023-12-18 08:17:57 -0500

    Add placeholder dashboard homepage

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col items-center text-center justify-center h-[100vh]">
+      <div className="text-4xl font-semibold">Welcome to the dashboard!</div>
+    </div>
+  );
+}
