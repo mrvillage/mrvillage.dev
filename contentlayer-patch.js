36f3cbd95de24556f738d7e755712ef8dd9ebe5f
mrvillage <josef@mrvillage.dev>
2023-12-21 18:09:47 -0500

    Make patch file cleaner

--- contentlayer-patch.js
+++ contentlayer-patch.js
@@ -1,29 +1,24 @@
 const { mkdirSync, existsSync, writeFileSync, readFileSync } = require("fs");
 
-if (!existsSync("./content-dist")) {
-  mkdirSync("./content-dist");
-}
-
 const types = ["Author", "Post"];
 
 for (const t of types) {
-  const path = `./.contentlayer/generated/${t}/_index.json`;
-  const contents = readFileSync(path).toString();
-  const allDocuments = JSON.parse(contents);
+  const documents = JSON.parse(
+    readFileSync(`./.contentlayer/generated/${t}/_index.json`).toString()
+  );
 
-  // const allDocuments: any[] = [];
-  for (const doc of allDocuments) {
+  for (const doc of documents) {
     const path = `${doc._raw.flattenedPath}.js`;
     const dir = path.split("/").slice(0, -1).join("/");
     if (!existsSync(`./content-dist/${dir}`)) {
       mkdirSync(`./content-dist/${dir}`, { recursive: true });
     }
-    // export default function Document(${Object.keys(components).join(", ")}) {
-    const content = `
-export default function Document(React, ReactDOM, _jsx_runtime) {
+    writeFileSync(
+      `./content-dist/${path}`,
+      `export default function Document(React, ReactDOM, _jsx_runtime) {
   ${doc.body.code}
 }
-`;
-    writeFileSync(`./content-dist/${path}`, content);
+`
+    );
   }
 }
