76389676baffd061bf99ddf89d6d820fffc34a17
mrvillage <josef@mrvillage.dev>
2023-12-26 23:11:09 -0500

    Add back button to blog list pages

--- components/blog-list-page.tsx
+++ components/blog-list-page.tsx
@@ -1,5 +1,9 @@
 import { Post } from "@/.contentlayer/generated";
 import { BlogPostList } from "./blog-post-list";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import * as Icons from "@/components/icons";
 
 interface BlogListPageProps {
   posts: Post[];
@@ -9,7 +13,16 @@ interface BlogListPageProps {
 
 export function BlogListPage({ posts, title, description }: BlogListPageProps) {
   return (
-    <div className="container max-w-4xl py-6 lg:py-10">
+    <div className="container relative max-w-4xl py-6 lg:py-10">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-[-150px] top-14 hidden xl:inline-flex"
+        )}
+      >
+        <Icons.Back className="mr-2 h-4 w-4" /> Home
+      </Link>
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 gap-y-4">
           <h1 className="inline-block font-heading font-semibold text-4xl tracking-tight lg:text-5xl">
