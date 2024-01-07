1591e1e1a7c4afc82c6a8e19f513193351713b0e
mrvillage <josef@mrvillage.dev>
2024-01-07 05:55:09 -0500

    Add a bunch of simple links

---
+++ app/email/page.tsx
@@ -0,0 +1,6 @@
+import { EMAIL_LINK } from "@/lib/consts";
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect(EMAIL_LINK);
+}
---
+++ app/v19/page.tsx
@@ -0,0 +1,6 @@
+import { V19_LINK } from "@/lib/consts";
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect(V19_LINK);
+}
