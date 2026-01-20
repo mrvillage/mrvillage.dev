cd0397d86842cdc056b8a934c1483e83a0f0ed3b
mrvillage <josef@mrvillage.dev>
2026-01-20 09:52:00 -0500

    Update projects

--- lib/env.ts
+++ lib/env.ts
@@ -3,16 +3,12 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    github_client_id: z.string().min(1),
-    github_client_secret: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
     NEXT_PUBLIC_IS_DEV: z.string().optional(),
   },
   runtimeEnv: {
-    github_client_id: process.env.GITHUB_CLIENT_ID,
-    github_client_secret: process.env.GITHUB_CLIENT_SECRET,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_IS_DEV: process.env.NEXT_PUBLIC_IS_DEV,
   },
